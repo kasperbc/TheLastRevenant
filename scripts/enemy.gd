52fd18e56c90ead99fce716cda76bda2ab484491
Kasper <kasper.bjorkgren@gmail.com>
2023-11-27 17:21:24 +0200

    Enemy health / damage

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,7 +3,7 @@ class_name Enemy
 
 @onready var player = GameMan.get_player()
 
-@export var health : int
+@export var health : int = 1
 
 func _on_player_attached():
 	player_attached()
@@ -17,7 +17,7 @@ func player_attached():
 	damage_player()
 
 func player_attacked():
-	die()
+	take_damage()
 
 func damage_player():
 	player.hook_released.emit()
@@ -27,6 +27,15 @@ func damage_player():
 func knock_back_player(amount : Vector2):
 	player.velocity.x = (position.direction_to(player.position) * amount.x).x
 	player.velocity.y = amount.y
+
+func take_damage():
+	health -= 1
+	
+	if health == 0:
+		die()
+		return
+	else:
+		knock_back_player(Vector2(200, -300))
 
 func die():
 	player.hook_released_early.emit()
