23352740f337174b6509670451985a119e5a1fcf
Kasper <kasper.bjorkgren@gmail.com>
2023-11-28 15:12:38 +0200

    health + enemy changes

--- player/player_health.gd
+++ player/player_health.gd
@@ -1,15 +1,35 @@
 extends Node
 
-var health_upgrades = 0
-@onready var current_health = BASE_MAX_HEALTH
-
 const BASE_MAX_HEALTH = 4
 
-signal on_health_change
+var _current_health
+var current_health : 
+	get:
+		return _current_health
+	set(value):
+		_current_health = value
+		get_tree().root.get_node("/root/Main/UI/Control/HealthLabel").text = "Health %s" % current_health
+
+var health_upgrades = 0
+var invincible = false
+
+
+func _ready():
+	current_health = BASE_MAX_HEALTH
 
 func damage():
+	if invincible:
+		return
+	
 	current_health -= 1
 	print("Player took damage: current health: %s" % current_health)
 	
 	if current_health <= 0:
 		GameMan.reload_scene()
+	
+	invincible = true
+	$InvincibilityTimer.start()
+
+
+func _on_invincibility_timer_timeout():
+	invincible = false
