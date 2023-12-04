c3d760504cf997f535afccfd3af7577c4034d4e5
Kasper <kasper.bjorkgren@gmail.com>
2023-12-04 23:25:10 +0200

    Electric walls

---
+++ objects/enemies/electric_wall/electric_wall.gd
@@ -0,0 +1,20 @@
+extends Enemy
+class_name ElectricWall
+
+func _ready():
+	$Sprite2D.scale.y = 1 / scale.y
+	$Sprite2D.region_rect.size.y = 16 * scale.y
+
+func _process(delta):
+	super(delta)
+	set_collision_layer_value(1, GameMan.get_upgrade_status(GameMan.Upgrades.GALVANIC_MODULE) != GameMan.UpgradeStatus.ENABLED)
+	set_collision_mask_value(2, GameMan.get_upgrade_status(GameMan.Upgrades.GALVANIC_MODULE) != GameMan.UpgradeStatus.ENABLED)
+
+func on_player_contact():
+	if GameMan.get_upgrade_status(GameMan.Upgrades.GALVANIC_MODULE) == GameMan.UpgradeStatus.ENABLED:
+		return
+	
+	damage_player()
+
+func on_player_attacked():
+	pass
