052b8b78041e43c73d17222f8a10ecd11ac48fd5
Kasper <kasper.bjorkgren@gmail.com>
2023-12-05 16:41:22 +0200

    finished easy drones

--- scripts/enemy/ai_states/fly_and_turn_towards_player.gd
+++ scripts/enemy/ai_states/fly_and_turn_towards_player.gd
@@ -3,21 +3,15 @@ class_name FlyAndTurnTowardsPlayer
 
 @export var turn_speed = 5.0
 
-var target_point = Vector2.ZERO
-
-func _on_state_activate():
-	super()
-	target_point = GameMan.get_player().global_position
-
 func ai_state_process(delta):
 	super(delta)
 	
 	move_target_point(delta)
-	rotate_towards_target_point()
 
 func move_target_point(delta):
-	target_point.move_toward(GameMan.get_player().global_position, delta * turn_speed)
+	var target_angle = rad_to_deg(Vector2.RIGHT.angle_to(body.global_position.direction_to(GameMan.get_player().global_position)))
+	body.rotation_degrees = rotate_toward(body.rotation_degrees, target_angle, turn_speed * delta)
+	dir_towards_rotate()
 
-func rotate_towards_target_point():
-	var angle = rad_to_deg(Vector2.RIGHT.angle_to(body.global_position.direction_to(target_point)))
-	body.rotation_degrees = angle
+func dir_towards_rotate():
+	dir = Vector2.from_angle(deg_to_rad(body.rotation_degrees))
--- scripts/rotate_towards_player.gd
+++ scripts/rotate_towards_player.gd
@@ -1,4 +1,7 @@
 extends Node2D
+class_name RotateToPlayer
+
+@export var rotation_speed = 50.0
 
 func _process(delta):
-	rotation_degrees = rad_to_deg(Vector2.RIGHT.angle_to(global_position.direction_to(GameMan.get_player().global_position)))
+	rotation = rotate_toward(rotation, Vector2.RIGHT.angle_to(global_position.direction_to(GameMan.get_player().global_position)), delta * rotation_speed)
