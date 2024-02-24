10977bc4739c5ca93389b0b86ab11a67d02431a3
Kasper <kasper.bjorkgren@gmail.com>
2024-02-24 11:31:17 +0200

    fixed stuffs

--- objects/enemies/boxes/flesh_box.gd
+++ objects/enemies/boxes/flesh_box.gd
@@ -1,6 +1,9 @@
 extends Enemy
+class_name DestroyableBox
+
+@export var flesh = true
 
 func take_damage():
-	if GameMan.get_upgrade_status(GameMan.Upgrades.THERMAL_MODULE) != GameMan.UpgradeStatus.ENABLED:
+	if GameMan.get_upgrade_status(GameMan.Upgrades.THERMAL_MODULE) != GameMan.UpgradeStatus.ENABLED and flesh:
 		return
 	super()
